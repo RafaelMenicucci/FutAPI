1df6ed4bcf4680ec75177b6835a6c0d3e5604cd1
Rafael Alves <rafa_alves96@hotmail.com>
2025-09-21 00:09:24 -0300

    fixing static

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views.apiView import get_jogos, post_jogos
 
 urlpatterns = [
-    path('jogos/', get_jogos, name='get_jogos'),
-    path('criar/jogo/', post_jogos, name='post_jogos'),
-]
+    path("jogos/", get_jogos, name="get_jogos"),
+    path("criar/jogo/", post_jogos, name="post_jogos"),
+] + static(settings.STATIC_URL)
