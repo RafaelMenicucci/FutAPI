98c3a105aa604918e64b2a594dc626859b41390c
Rafael Alves <rafa_alves96@hotmail.com>
2025-09-20 23:36:12 -0300

    fixing apps

--- api/views/apiView.py
+++ api/views/apiView.py
@@ -1,12 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from ..models.jogos import Jogos
-from ..serializers.jogosSerializer import JogosSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAdminUser
+from ..models.jogos import Jogos
+from ..serializers.jogosSerializer import JogosSerializer
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_jogos(request):
     print(request.user.is_staff)
@@ -14,7 +15,8 @@ def get_jogos(request):
     serializer = JogosSerializer(jogos, many=True)
     return Response(serializer.data)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 @permission_classes([IsAuthenticated, IsAdminUser])
 def post_jogos(request):
     serializer = JogosSerializer(data=request.data)
@@ -22,4 +24,3 @@ def post_jogos(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
-        
