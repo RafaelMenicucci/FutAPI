93a1f6cd66e6f5ab8a74ab0e7a02347a9ca87fe0
Rafael Alves <rafa_alves96@hotmail.com>
2025-09-21 14:27:18 -0300

    fix static and build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
-python manage.py collectstatic
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic
 
 echo "Vercel build process completed."
