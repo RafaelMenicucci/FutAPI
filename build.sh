6c94d3619663c2a4140956086c0e6b5ab56d7dae
Rafael Alves <rafa_alves96@hotmail.com>
2025-09-21 16:10:45 -0300

    fix build

--- build.sh
+++ build.sh
@@ -6,6 +6,13 @@ mkdir staticfiles
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# Apply database migrations
+echo "Making Migrations..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echo "Collecting Static Files..."
 python3 manage.py collectstatic
 
 echo "Vercel build process completed."
