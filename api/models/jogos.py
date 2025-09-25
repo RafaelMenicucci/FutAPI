4afed4b9a070fc89eaeeeeef007550c4dfe53229
Rafael Alves <rafa_alves96@hotmail.com>
2025-09-25 18:58:17 -0300

    fix order

--- api/models/jogos.py
+++ api/models/jogos.py
@@ -2,24 +2,24 @@ from django.db import models
 
 
 class Jogos(models.Model):
+    ano = models.IntegerField()
+    rodada = models.IntegerField()
     time_casa = models.CharField(max_length=30)
     time_fora = models.CharField(max_length=30)
     gols_casa = models.IntegerField(blank=True, null=True)
     gols_fora = models.IntegerField(blank=True, null=True)
-    ano = models.IntegerField()
-    rodada = models.IntegerField()
-    amarelo_casa = models.IntegerField(blank=True, null=True)
-    amarelo_fora = models.IntegerField(blank=True, null=True)
-    vermelho_casa = models.IntegerField(blank=True, null=True)
-    vermelho_fora = models.IntegerField(blank=True, null=True)
-    escanteio_casa = models.IntegerField(blank=True, null=True)
-    escanteio_fora = models.IntegerField(blank=True, null=True)
-    faltas_casa = models.IntegerField(blank=True, null=True)
-    faltas_fora = models.IntegerField(blank=True, null=True)
     chutes_casa = models.IntegerField(blank=True, null=True)
     chutes_fora = models.IntegerField(blank=True, null=True)
     defesa_casa = models.IntegerField(blank=True, null=True)
     defesa_fora = models.IntegerField(blank=True, null=True)
+    escanteio_casa = models.IntegerField(blank=True, null=True)
+    escanteio_fora = models.IntegerField(blank=True, null=True)
+    faltas_casa = models.IntegerField(blank=True, null=True)
+    faltas_fora = models.IntegerField(blank=True, null=True)
+    amarelo_casa = models.IntegerField(blank=True, null=True)
+    amarelo_fora = models.IntegerField(blank=True, null=True)
+    vermelho_casa = models.IntegerField(blank=True, null=True)
+    vermelho_fora = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return (
