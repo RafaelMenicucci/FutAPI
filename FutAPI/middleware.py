4a8f2e12626e1cd6c207cf6f487e94a0d42ee4df
Rafael Alves <rafa_alves96@hotmail.com>
2025-10-08 18:36:17 -0300

    fix middleware

--- FutAPI/middleware.py
+++ FutAPI/middleware.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.utils.deprecation import MiddlewareMixin
@@ -10,4 +9,4 @@ class ProcessRequestMiddleware(MiddlewareMixin):
             user = request.user
             if not user.is_authenticated:
                 messages.error(request, "Your need to log in to access this page!")
-                return redirect("home")
+                return redirect("signin")
